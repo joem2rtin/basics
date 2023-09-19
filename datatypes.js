81fe13f02e662b7944fda9122837e995c25b6d79
Martin Joe <martin.joe@voco.ee>
2023-09-19 13:55:44 +0300

    data types

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+// data types
+// String
+let firstname = 'Martin'
+let lastname = 'Joe'
+
+console.log(firstname + '' +lastname)
+console.log(typeof firstname)
+
+// Numbers
+let goodMark = 5
+console.log(typeof goodMark)
+
+let percent = 50.3
+console.log(typeof percent)
+
+// Boolean
+let isToday = true
+console.log(typeof isToday)
+
+//type conversions
+// number -> string
+console.log( typeof String (555))
+console.log( String (5+5))
+console.log(String(5.5) + String(5.5))
+
+// boolean
+console.log(String(true))
+
+//String -> number
+console.log(number('666'))
+console.log(number('66.6'))
+console.log(parseINT(111.3))
+console.log(parseFloat(111.3))

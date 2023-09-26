9625677f36e81bc018a422a3e6b5acd02fb2f048
Martin Joe <martin.joe@voco.ee>
2023-09-26 12:07:51 +0300

    basic math

---
+++ math.js
@@ -0,0 +1,26 @@
+let number1 = 523
+let number2 =333
+
+let result
+
+result = number1 + number2
+result = number1 - number2
+result = number1 * number2
+result = number1 / number2
+
+result = Math.PI
+result = Math.round (555.9)
+	result = Math.round (100.2)
+
+let value1 = Math.floor (111.6) //down
+let value2 = Math.ceil (111.3) //up
+
+result= Math.sqrt(64)
+result=Math.abs(-3)
+result= Math.pow(8, 2)
+
+result=Math.random()
+
+result=Math.round(Math.random()*10)
+console.log (result)
+
